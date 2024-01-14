88bc9698506a680c9776afdc2e61c4ac3e8f04bf
Greg <Jariye05@gmail.com>
2024-01-14 03:18:41 -0800

    files and start

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import InvControl from "./InvControl"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <InvControl />
+    </>
+  );
+}
+
+export default App;
