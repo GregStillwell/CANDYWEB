a4468ee3bd7278edeb426ce81dd1285982377a36
Greg <Jariye05@gmail.com>
2024-01-20 22:39:51 -0800

    updated inventory.js

---
+++ src/Components/Inventory.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Inventory(props) {
+  return (
+    <React.Fragment>
+      <h1>{props.candy}:</h1>
+      <h3>Candy you bought cost {props.price}.</h3>
+      <p>{props.description}</p>
+    </React.Fragment>
+  );
+}
+
+Inventory.propTypes = {
+  candy: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired
+}
+
+export default Inventory;
