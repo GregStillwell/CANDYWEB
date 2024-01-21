689f2298cc848cb9937623314443ea3bc3aa6bc9
Greg <Jariye05@gmail.com>
2024-01-21 00:53:36 -0800

    inventory updated

--- src/Components/InvControl.js
+++ src/Components/InvControl.js
@@ -1,3 +1,53 @@
 import React from "react";
 import Inventory from "./Inventory";
 import InvList from "./InvList";
+import NewInvForm from "./NewInvForm";
+
+class InvControl extends React.Component {
+
+
+constructor(props) {
+  super(props);
+  this.state = {
+    formVisibleOnPage: false,
+    mainInvList: []
+  };
+}
+
+handleAddingNewInvToList = (newInv) => {
+  const newMainInvList = this.state.mainInvList.concat(newInv);
+  this.setState({
+    mainInvList: newMainInvList,
+    formVisibleOnPage: false
+  });
+}
+
+handleClick = () => {
+  this.setState(prevState => ({
+    formVisibleOnPage: !prevState.formVisibleOnPage
+  }));
+}
+
+render(){
+  let currentlyVisibleState = null;
+  let buttonText = null;
+  if (this.state.formVisibleOnPage) {
+    currentlyVisibleState = <NewInvForm onNewInvCreation={this.handleAddingNewInvToList} />
+    buttonText="Candy";
+  } else {
+    currentlyVisibleState = <InvList invList={this.state.mainInvList} />
+    buttonText="More candy";
+  }
+  return (
+    <React.Fragment>
+      <div className="content">
+        <p></p>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
+      </div>
+    </React.Fragment>
+  );
+}
+}
+
+export default InvControl;
