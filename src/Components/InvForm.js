f029af3d82d9a95ae75575e055218122b9e0e220
Greg <Jariye05@gmail.com>
2024-01-21 01:12:35 -0800

    updated invform

---
+++ src/Components/InvForm.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function InvForm(props) {
+  return (
+    <>
+    <form>
+      <input 
+      type="text"
+      name="Candy"
+      placeholder="Candy"
+      required />
+      <br />
+      <input 
+      type="text"
+      name="price"
+      placeholder="PRICE"
+      required />
+      <br />
+      <input 
+      type="text"
+      name="description"
+      placeholder="DESCRIPTION"
+      required />
+      <br />
+      <input 
+      type="number"
+      name="quantity"
+      min="0"
+      max="500"
+      defaultValue="130"
+      placeholder="QUANTITY"
+      required />
+      <br />
+      <button type="submit"></button>
+    </form>
+    </>
+  )
+}
